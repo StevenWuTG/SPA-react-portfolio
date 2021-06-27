af7f7affb536d72f5facedb47fd6efbb1925a9b9
StevenWuTG <stevenwu91@icloud.com>
2021-06-27 11:01:14 -0400

    imported emailjs api | functions ready

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,64 @@
 import React from 'react'
 import './contact.scss'
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+
+    function sendEmail(e){
+        e.preventDefault()
+
+        emailjs.sendForm('Gmail', 'template_aqdx4iw', e.target, 'user_9g8wroGHVK37aC43tSjIw')
+        .then((result) => {
+            console.log(result.text)
+        }, (error) => {
+            console.log(error.text)
+        });
+        alert("message sent")
+        e.target.reset()
+
+    }
+
+
     return (
         <div className="contact" id="contact">
-            contact
+            
+            <div className="form-box">
+
+                <div className="contact-icons">
+
+                    <a href="https://www.linkedin.com/in/stevenwubc/">
+                    <img src="https://img.icons8.com/fluent/240/000000/linkedin.png" 
+                    alt="linkedin" align="left" width="60px" height="60"></img>
+                    </a>
+
+                    <a href="https://github.com/StevenWuTG">
+                    <img src="https://img.icons8.com/fluent/240/000000/github.png" 
+                    alt="linkedin" align="left" width="60px" height="60"></img>
+                    </a>
+
+                    
+                    <a href="https://stevenwu91.medium.com/">
+                    <img src="https://img.icons8.com/ios-filled/250/000000/medium-monogram--v1.png" 
+                    alt="linkedin" align="left" width="60px" height="60"></img>
+                    </a>
+                    
+
+                </div>
+                
+                <form className="contact-form" onSubmit={sendEmail}>
+                    {/* <input type="hidden" name="contact_number" /> */}
+                    <label>Subject:</label>
+                    <input type="text" name="subject" />
+                    <label>Name:</label>
+                    <input type="text" name="name" />
+                    <label>Email:</label>
+                    <input type="email" name="email" />
+                    <label>Message:</label>
+                    <textarea name="message" />
+                    <input className="btn btn-primary"  type="submit" value="Send" />
+                </form>
+            </div>
+
         </div>
     )
 }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -11,11 +11,13 @@ const Intro = () => {
                 
                 <br></br>
                 
-                <p>As a avid gamer, it has always been one of my dreams to learn how to program and I fell in love after writing my first few lines of code. A day later, I've decided to turn my dreams into reality by enrolling in The Flatiron School's Immersive Software Engineering bootcamp.</p>
-                <br />
+                <p>As an avid gamer, it has always been one of my dreams to learn how to program and I fell in love after writing my first few lines of code. A day later, I've decided to turn my dreams into reality by enrolling in The Flatiron School's Immersive Software Engineering bootcamp.</p>
+                {/* <br /> */}
                 <p>Along with my coding knowledge, I have over twelve years of experience in the restaurant industry with the last six years as a manager for a busy ramen restaurant in midtown Manhattan where I handled duties such as employee scheduling, inventory management and the daily front house operation.</p>
                 <br />
-                <h1>Skills:</h1>  
+                <br />
+                <h1>Languages:</h1>
+                <br />
                 <div className="intro-icons">
                     <img src="https://img.icons8.com/color/48/000000/html-5--v1.png" alt="html" align="left" width="40" height="40"/>
 
@@ -28,11 +30,12 @@ const Intro = () => {
                     <img src="https://img.icons8.com/officel/80/000000/ruby-programming-language.png" alt="ruby" align="left" width="40" height="40"/>
                     <img src="https://img.icons8.com/color/144/000000/postgreesql.png" alt="postgres" align="left" width="40" height="40"/>
                 </div>
+                {/* <br /> */}
                 
                 <Typed
                     className="typed-text"
-                    strings={["Web Design", "React UI", "RESTful Apis", "JavaScript", "Ruby"]}
-                    typeSpeed={80}
+                    strings={["Web Design", "React UI", "RESTful Apis", "JavaScript", "Ruby on Rails"]}
+                    typeSpeed={100}
                     backSpeed={40}
                     loop={true}
                 />
